f811a4b95e75e2debb6a679be5b65a80f4bda8f2
WANGVINCENT <wangnan.vincent@gmail.com>
2014-06-02 22:50:40 +0100

    add installation of multipart module in PigDebugger-config.sh

--- configuration/PigDebugger-config.sh
+++ configuration/PigDebugger-config.sh
@@ -1,19 +1,30 @@
 
 #config node.js
-echo "Configuring nodejs"
+echo "Configuring node.js"
 sudo apt-get install python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install nodejs
+echo "Installation of node.js succeeds!"
 
+echo "Install socket.io module"
 sudo npm install socket.io
+echo "Install mysql module"
 sudo npm install mysql
 
+echo "Install mysql server"
 sudo apt-get install mysql-server
 
+echo "Install shell terminal"
 #install shell terminal
-cd /home/hduser/PigDebugger/shellinabox-2.14
+cd ~/PigDebugger/shellinabox-2.14
 ./configure
 sudo make
 sudo make install
 
+echo "Install multipart module"
+#install multipart
+sudo apt-get install git
+cd ~/PigDebugger/node_modules
+git clone https://github.com/isaacs/multipart-js.git
+mv multipart-js multipart
